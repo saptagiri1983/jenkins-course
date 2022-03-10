3a450f65214e7da1b5fe1cd4fbfff6f597f6492a
saptagiri1983 <36218422+saptagiri1983@users.noreply.github.com>
2022-03-10 11:10:11 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,3 +1,17 @@
 job('NodeJS example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
     
-       }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    
+    steps {
+        shell ('echo helloWorld')
+    }
+}
