71d4c634cb46ef3517dcde67d54056326db4e29a
saptagiri1983 <36218422+saptagiri1983@users.noreply.github.com>
2022-03-10 11:01:50 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,10 +1,7 @@
 job('NodeJS example') {
     
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
+   
     steps {
-        shell("npm install")
+        shell("echo hello")
     }
 }
